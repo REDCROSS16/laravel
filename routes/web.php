17098e10ea13bed7fec51dccb0877f277ecdc693
redcross16 <belkill@mail.ru>
2022-01-07 23:08:32 +0300

    upd laravel project

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,24 @@ Route::get('/', function () {
 
 
 Route::get('/hello', function () {
-    return '<h1>Hello world</h1>';
+    return view('hello', [
+        'name' => 'Alexander',
+//        'skills' => ['HTML5', 'CSS3', 'VueJS', 'PHP' ],
+        'skills' => [],
+    ]);
+});
+
+Route::get('/test', function () {
+    return view('test');
 });
 
 
-Route::get('/post/{id}', function ($id) {
-   return 'page id = ' . $id ;
-});
 
-
-Route::get('/first-page', function () {
-    return '<h1 style="font-family: Arial;">This is my first page in Laravel</h1>';
-});
+//Route::get('/post/{id}', function ($id) {
+//   return 'page id = ' . $id ;
+//});
+//
+//
+//Route::get('/first-page', function () {
+//    return '<h1 style="font-family: Arial;">This is my first page in Laravel</h1>';
+//});
